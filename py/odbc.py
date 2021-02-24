f82fa78168b7a3229888d3a480deb26d9ff10eaf
Mitch Alves <michel.alves@hcahealthcare.com>
2021-02-24 08:06:55 -0500

    Create odbc.py

---
+++ py/odbc.py
@@ -0,0 +1,37 @@
+# =====================================================================================================================================
+# AUTHOR:   Mitch Alves (idp7116)
+# DATE:     2021-02-23
+# DESC:     APP EXECUTION - QUERY DB
+# DRIVERS:  https://docs.microsoft.com/en-us/sql/connect/odbc/download-odbc-driver-for-sql-server?view=sql-server-ver15
+#
+# =====================================================================================================================================
+
+import pyodbc 
+
+class odbc:
+  def __init__(self, server, database):
+    self.server = server or 'EFDVWDDBSSQL16.hca.corpad.net'
+    self.database = database or 'EDW_ADHOC'
+    self.uid = 'HCA\EFDVSVC_APP_BI'
+    self.pwd = 'y2NxCgG3'
+
+  def connection(self):
+    return 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+self.server+';DATABASE='+self.database+';UID='+self.uid+';PWD='+ self.pwd +';Trusted_connection=yes'
+   
+
+# ========================================================================================
+# TEST
+# ========================================================================================
+"""
+db = odbc(None,None)
+sql = 'INSERT INTO dbo.test (ID,Description) VALUES (?,?)'
+
+try:
+  print('Inserting...')
+  conn = pyodbc.connect(db.connection())
+  cursor = conn.cursor()
+  cursor.execute(sql,(3,'Test3'))
+  conn.commit()
+except pyodbc.Error as ex:
+  print(ex)
+"""
